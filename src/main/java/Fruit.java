ad5c8c2d59ccb74f0cb3111971693708db1a3283
AATrubakova <trubakovaaa@gmail.com>
2021-10-27 19:15:40 +0300

    Homework done

--- src/main/java/Fruit.java
+++ src/main/java/Fruit.java
@@ -1,2 +1,11 @@
 public class Fruit {
+    public Float weight;
+
+    public Fruit(Float weight) {
+        this.weight = weight;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
 }
