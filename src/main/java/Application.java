02ca874859b61cf96946610c9cec36b7ffb27eb1
AATrubakova <trubakovaaa@gmail.com>
2021-10-27 09:30:03 +0300

    Initial commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,7 +3,7 @@ public class Application {
 
         Integer[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         Swapper<Integer> integerSwapper = new Swapper<>();
-        integerSwapper.swapElementsArray(array, 3, 7);
+        integerSwapper.swapElementsArray(array, 2, 8);
 
         for(Integer i: array) {
             System.out.print(i +" ");
